00b84c6d1e84e6d0aa44eeae849dee40b1d8e772
Damian Posełek <damian.poselek1@gmail.com>
2020-04-10 16:01:09 +0200

    lotto

--- src/pl/coderslab/Game1/Game_1.java
+++ src/pl/coderslab/Game1/Game_1.java
@@ -1,4 +1,40 @@
 package pl.coderslab.Game1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game_1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);                     // Otrzymuję dostęp do terminala
+
+        Random r = new Random();                                   // Wylosuj losową liczbę
+        int randomNumber = r.nextInt(100 + 1) - 1;         // Przypisz losową liczbę do randomNumbers
+
+        while (true) {
+
+            System.out.print("Zgadnij liczbę: ");
+            while (!scan.hasNextInt()) {                         // Jeśli wpisane znaki nie są liczbą to wtedy dzieją się dwie rzeczy: ( "!" oznacza zaprzeczenie
+                scan.nextInt();                                  // 1. Polecenia wpisania liczb w konsoli
+                System.out.println("To nie jest liczba!");       // 2. Wyświetla komunikat "To nie jest liczba
+            }
+
+            int myNumber = scan.nextInt();                       // Dopiero po pętli jeśli już mam pewność, ze jest to liczba, to przypisuję liczbę do myNumber
+
+            while (myNumber > 100 || myNumber < 1) {
+                System.out.println("Podana wartość musi być liczbą z przedziału 1-100");
+                myNumber = scan.nextInt();
+            }
+
+            if (myNumber < randomNumber) {                       // komunikaty które będą sprawdzały czy liczba jest za mała lub za duża albo WŁAŚCIWA :)
+                System.out.println("Za mało!");
+            } else if (myNumber > randomNumber) {
+                System.out.println("Za dużo!");
+            } else {
+                System.out.println("Zgadłeś! Szukana liczba to: " + randomNumber);
+                break;                                            // BREAK potrzebny do zatrzymania pętli
+            }
+        }
+    }
 }
--- src/pl/coderslab/Game2/Game_2.java
+++ src/pl/coderslab/Game2/Game_2.java
@@ -1,4 +1,39 @@
 package pl.coderslab.Game2;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game_2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);               // dostęp do terminala
+        int[] lottoNumbers = new int[6];
+
+
+        for (int i = 0; i < lottoNumbers.length; i++) {            // losowanie liczby lotto bez powtórzeń żadnej z liczb
+            System.out.print("Typowane liczby Lotto: ");
+            while (!scan.hasNextInt()) {                         // Jeśli wpisane znaki nie są liczbą to wtedy dzieją się dwie rzeczy: ( "!" oznacza zaprzeczenie
+                scan.nextLine();                                 // 1. Polecenia wpisania liczb w konsoli
+                System.out.println("To nie jest liczba!");       // 2. Wyświetla komunikat "To nie jest liczba!"
+                System.out.print("Typowane liczby Lotto: ");
+            }
+
+            int userNumber = scan.nextInt();
+
+            if (ArrayUtils.contains(lottoNumbers, userNumber)) {
+                System.out.println("Taka liczba już istnieje, wybierz ponownie.");
+                i--;
+            } else if (userNumber > 49 || userNumber < 1) {
+                System.out.println("Wybierz liczbę z przedziału 1-49");
+                i--;
+            } else {
+                lottoNumbers[i] = userNumber;
+            }
+        }
+        System.out.println(Arrays.toString(lottoNumbers));
+    }
 }
